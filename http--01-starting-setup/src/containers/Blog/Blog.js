eacb46ce395d9f9bf13f56518c3c4a14b9f165fd
markhorvath <markhorvath7@gmail.com>
2018-04-19 13:39:17 -0400

    lec183 using links to switch pages

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 // this is from the axios.js file, the instance, it could've been called "import instance"
 // but then you'd have to change all the times 'axios' was used in the component below
 //import axios from '../../axios';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from '../Posts/Posts';
@@ -19,8 +19,12 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: 'submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
