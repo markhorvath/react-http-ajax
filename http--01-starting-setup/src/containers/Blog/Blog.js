829dc7c5c06f3ce7f7b1c65fc396ecf1ee00b25c
markhorvath <markhorvath7@gmail.com>
2018-04-19 16:39:32 -0400

    lec186-188 absolute and relative paths and styling the active route

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 // this is from the axios.js file, the instance, it could've been called "import instance"
 // but then you'd have to change all the times 'axios' was used in the component below
 //import axios from '../../axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from '../Posts/Posts';
@@ -19,12 +19,21 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                    {/* exact added as a prop to NavLink here so that it can get the .active class */}
+                {/* activeClassName="active" is just there to show you can change the name to whatever you want,
+                here it's redundant because the default class name is already 'active' */}
+                            <li><NavLink
+                                to="/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{color: '#42f44b', textDecoration: 'underline'}}>Home</NavLink></li>
+                            {/* pathname: this.props.match.url + '/new-post' is the relative
+                        way of appending a new path to the current one*/}
+                            <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: 'submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
