04262be7e1b73c5ee9f4a2ab79613c54215eeda4
markhorvath <markhorvath7@gmail.com>
2018-04-20 16:41:09 -0400

    lec203 routing and server deployment, just left a comment in app.js about basename prop in browserrouter

--- http--01-starting-setup/src/App.js
+++ http--01-starting-setup/src/App.js
@@ -6,6 +6,9 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+    // you can set the basename prop in BrowserRouter, by default its set to '/'
+    // for example if you were hosting on github i think you'd set it to something like
+    // basename="github.com/markhorvath/my-app/"
       <BrowserRouter>
           <div className="App">
             <Blog />
