b6da60083cc47c3ae259cdd9a5c61b29072b2523
markhorvath <markhorvath7@gmail.com>
2018-05-02 09:52:52 -0400

    hoek vulnerability attempted fix

--- http--01-starting-setup/src/App.js
+++ http--01-starting-setup/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     // you can set the basename prop in BrowserRouter, by default its set to '/'
     // for example if you were hosting on github i think you'd set it to something like
     // basename="github.com/markhorvath/my-app/"
+    //<BrowserRouter basename="my-app" />
       <BrowserRouter>
           <div className="App">
             <Blog />
